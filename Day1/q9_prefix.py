9a69e02f1111a1841e06fe1b6484fd8db5218cdc
Aakash Rohila <53647802+aakashrohila@users.noreply.github.com>
2023-02-21 23:48:30 +0530

    Add files via upload

--- Day1/q9_prefix.py
+++ Day1/q9_prefix.py
@@ -2,11 +2,30 @@
 
 st = ['hello' , 'helloIamboy','helloIamgirl' , 'hello']
 
-mt_list = dict()
+mt_list = []
 
-for i in st:
-    mt_list[j] = 0
-    for j in i:
-        mt_list[j] = mt_list[j] + 1
+# Sorting the list in ascending order as the first element would be the max possible prefix
+st.sort()
 
-print(mt_list)
+#Checking
+for i in range(len(st[0])):
+    flag = 0
+    string = ''
+    for j in st:
+
+        if st[0][i] == j[i]:
+            string = string + j[i]
+
+        else:
+            flag = 1
+            break
+    
+    mt_list.append(len(string)) #['aaaa' , 'eeee' , 'pppp' , 'kkk'] --> [4,4,4,3]
+    
+    if flag == 1:
+        break
+
+print("The length of the longest common prefix in a list of strings is")
+
+print("-->",mt_list.count(max(mt_list)))
+        
